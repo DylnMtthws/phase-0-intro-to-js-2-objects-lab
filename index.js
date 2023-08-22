ae3abcc2243bd39f7f84c4f8470f6bf241fe86d5
dylanmatthews <djmatthews93@gmail.com>
2023-08-22 08:01:23 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Dylan", 
+    streetAddress: "3425 Tisdale",
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    return {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, name, streetAddress) {
+    const newEmployee = {...employee};
+
+    delete newEmployee.name;
+
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name, streetAddress) {
+    delete employee.name;
+
+    return employee;
+    };
